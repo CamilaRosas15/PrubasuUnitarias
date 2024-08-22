f3fd0d9d5edddd9878349d308c36ad5c987513b1
Camila Rosas <camila.rosas@ucb.edu.bo>
2024-08-22 09:14:14 -0400

    Generar un numero que no siga ninguna regla

---
+++ src/fizzbuzz.js
@@ -0,0 +1,19 @@
+//codigo de produccion
+/*
+function generarFizzBuzz()
+{
+    return "1";
+}*/
+
+function generarFizzBuzz(numero)
+{
+    if(numero == "3")
+        return "Fizz"
+    else{
+        return numero+"";    
+    }
+    //return numero+"";
+}
+
+
+export default generarFizzBuzz;
---
+++ src/fizzbuzz.spec.js
@@ -0,0 +1,17 @@
+import generarFizzBuzz from "./fizzbuzz";
+
+describe("FizzBuzz", () =>{
+    it("generar el mismo si no sigue ninguna regla", () => {
+        expect(generarFizzBuzz(1)).toEqual("1");
+    });
+    it("generar el mismo si para cualquier numero si no sigue ninguna regla", () => {
+        expect(generarFizzBuzz(2)).toEqual("2");
+    });
+    it("generar fizz si el numero es 3 o multiplos de 3 1mera regla", () => {
+        expect(generarFizzBuzz(3)).toEqual("Fizz");
+    });
+});
+
+//modificar esta prueda para que genere un numero
+
+
